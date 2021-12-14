ed83f15ba48009fbbe68be5dc1fbba217c92f813
Conehh <nemanja.nikolic401@gmail.com>
2021-12-14 03:45:17 +0100

    Register and login

--- pythonProject/app.py
+++ pythonProject/app.py
@@ -2,14 +2,12 @@ from app import app
 from app.db.models import User,Account,Transaction,Card
 
 
-@app.shell_context_processor()
+@app.shell_context_processor
 def create_shell_context():
     return {'User': User, 'Account': Account, 'Transaction': Transaction, 'Card': Card}
 
 
-def main():
-    app.run()
 
 
 if __name__ == "__main__":
-    main()
+    app.run(debug=True)
--- pythonProject/app/__init__.py
+++ pythonProject/app/__init__.py
@@ -2,9 +2,15 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from config import Config
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '2e9c84f951360f6a674007c6'
+
 app.config.from_object(Config)
+login_manager = LoginManager(app)
+login_manager.login_view = "login_page"
+login_manager.login_message_category="info"
 database = SQLAlchemy(app)
 migrate = Migrate(app, database)
 
