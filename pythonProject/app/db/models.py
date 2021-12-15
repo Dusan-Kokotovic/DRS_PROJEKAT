45ebd05d484fc4d1ad9a75606fb8638489f7a3f7
Nikola <nikolaanicic99@gmail.com>
2021-12-15 16:14:37 +0100

    ispravka modela transakcije

--- pythonProject/app/db/models.py
+++ pythonProject/app/db/models.py
@@ -41,6 +41,7 @@ class Transaction(db.Model):
 
     sender_id = db.Column(db.Integer(), db.ForeignKey('user.id'))
     receiver_id = db.Column(db.Integer(), db.ForeignKey('user.id'))
+    datetime = db.Column(db.DateTime(), nullable=False)
 
     def __repr__(self):
         return f'Transaction: {self.id}'
--- pythonProject/migrations/env.py
+++ pythonProject/migrations/env.py
@@ -7,7 +7,7 @@ from flask import current_app
 
 from alembic import context
 
-from app.db.models import User, Transaction, Account, Card
+from app.db.models import User, Transaction, Card, Account
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
